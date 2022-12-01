886cd5b28063b15900e05266addacb949a036423
sadowbass <shinwa46@naver.com>
2022-12-01 14:45:02 +0900

    redirect http to https

---
+++ src/main/java/com/momc/momc/config/MultiConnectorConfig.java
@@ -0,0 +1,41 @@
+package com.momc.momc.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MultiConnectorConfig {
+
+    @Bean
+    public ServletWebServerFactory servletcontainer() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcatServletWebServerFactory;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(80);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
